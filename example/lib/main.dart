16a3846fa93880e440681c50378fef7df2a1983c
Hosea Tao <howshea@users.noreply.github.com>
2021-12-01 15:13:46 +0800

    Update README CN

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Smooth Corner',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'SMOOTH CORNER'),
     );
   }
 }
